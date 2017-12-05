21de88f3f8037369984ba1d53795ded53fb026b5
rzeznia <marcin.gd3@gmail.com>
2017-12-05 20:47:18 +0100

    Dodano trójeczkę

---
+++ Laboratorium/Zadanie3/index.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+<html lang ="pl">
+<!--Napisz skrypt z wykorzystaniem funkcji, czy wprowadzona przez
+użytkownika liczba (w polu formularza) jest podzielna przez 7,
+odpowiednio wyświetlając napis: "Liczba … jest podzielna przez 7" lub
+"Liczba … nie jest podzielna przez 7". Uwzględnij brak podania liczby..-->
+
+<head>   
+</head>
+<body>
+    <h1>Sprawdzenie podzielności liczby przez 7 Z FUNKCJĄ</h1>
+    <form method="POST" action="sprawdzenie.php">
+        Wprowadź liczbę
+        <input type="text" name="liczba">
+        <input type="submit" value="Sprawdź to">
+    </form>
+    <?php
+    ?>
+</body>
+</html>
